1a967c0a7c8cedd844b36eea801289cc1401e55e
adharainfinita <adharanosalevich@gmail.com>
2025-08-14 22:35:41 -0300

    structure archives and directories

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import { BooksModule } from './modules/books/books.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost', 
+    port: 5432,
+    username: 'your_username',
+    password: 'your_password', 
+    database: 'your_database',
+    autoLoadEntities: true,
+    synchronize: true,
+  }), UsersModule, BooksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
